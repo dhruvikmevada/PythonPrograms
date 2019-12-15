6e7dfc2e2d8734dfb713e3f3590305fe02897037
Dhruvik Mevada <dhruvikmevada@users.noreply.github.com>
2019-12-15 18:42:38 +0530

    [Add] ipCheck.py using regular expression

---
+++ ipCheck.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import subprocess
+import datetime
+import re 
+
+
+class ipcheck():
+
+    def ip_check(self):
+
+        ip = input('Enter your IP address: ')
+        
+        #  Checking the IP address format using regular expression
+        #  \d = match any digit in given string. \d{1,3} - match digits upto 3 counts only
+        if not re.match(r'(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})', ip) or any(a not in range(0,256) for a in map(int, ip.split('.'))):
+                print('Invalid')
+                # print('Not valid')
+        else:
+            print(ip)
+
+def main():
+
+    ipCheck = ipcheck()
+    ipCheck.ip_check()
+
+if __name__ == "__main__":
+    main()
